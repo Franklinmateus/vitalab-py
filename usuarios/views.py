b1ff046d023e5642d5ffdf2d77b6ddd2d85ea758
Franklinmateus <frankmateus660@gmail.com>
2023-10-06 21:00:13 -0300

    criando exames

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User 
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 def cadastro(request):
     if request.method == 'GET':
@@ -38,4 +39,19 @@ def cadastro(request):
             
         return redirect('/usuarios/cadastro')
 
+def logar(request):
+    if request.method == "GET":
+        return render (request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = authenticate(username=username, password=senha)
+        
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.add_message(request, constants.ERROR, "Username ou senhas inválidos!")
+            return redirect('/usuarios/login')
         
